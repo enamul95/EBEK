83bd1ae166d6861d4ebcf87acd054997b1a784ce
Enamul Haque <Enamul@enamuls-mac-mini.local>
2016-11-24 10:56:57 +0600

    Initial Commit

---
+++ EBEK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EBEK
+//
+//  Created by Enamul Haque on 11/24/16.
+//  Copyright © 2016 Enamul Haque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
